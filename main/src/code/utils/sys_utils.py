ce1c87f966c0e5af0dc2030147298c3fdcd3f2e6
Sreeram R <rsreeram17@gmail.com>
2021-04-29 17:05:50 +0530

    Updated the root path

--- main/src/code/utils/sys_utils.py
+++ main/src/code/utils/sys_utils.py
@@ -4,7 +4,7 @@ from functools import reduce
 
 def reduce_get(cfg, key):
 
-    return reduce(lambda c, k: c[k], key.split(".", cfg))
+    return reduce(lambda c, k: c[k], key.split("."), cfg)
 
 class ConfigClass(object):
     def __init__(self, cfg: dict):
@@ -13,7 +13,7 @@ class ConfigClass(object):
     def get(self, key):
         return reduce_get(self.cfg, key)
 
-def read_config(config_name: str, root_path="../../../..") -> ConfigClass:
+def read_config(config_name: str, root_path="../../..") -> ConfigClass:
 
     yaml_file_name = "{}.yaml".format(config_name)
     opj = os.path.join

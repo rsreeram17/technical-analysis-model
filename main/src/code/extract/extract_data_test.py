3b68e5395a332610c83cca22ddff19c426112a99
Sreeram R <rsreeram17@gmail.com>
2021-04-30 14:01:19 +0530

    Update extract_data_test.py

--- main/src/code/extract/extract_data_test.py
+++ main/src/code/extract/extract_data_test.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 from main.src.code.utils.fmp_python.fmp import FMP
 from main.src.code.utils.sys_utils import read_config
-from main.src.code.utils.utils import identify_os, extract_folder_path, get_yaml_value
+from main.src.code.utils.utils import extract_folder_path, get_yaml_value
 
 
 opj = os.path.join

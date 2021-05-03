7d125b9ab38ac7b3fd25038e1f6a4fb229362f93
Sreeram R <rsreeram17@gmail.com>
2021-05-03 15:46:39 +0530

    Create create_lagged_features and cache_info functions

--- main/src/code/utils/utils.py
+++ main/src/code/utils/utils.py
@@ -1,5 +1,7 @@
 import os
 from main.src.code.utils.sys_utils import read_config
+import pandas as pd
+import json
 
 def get_yaml_value(filename: str, key: str):
 
@@ -19,3 +21,29 @@ def extract_folder_path(type_):
     return output_folder_path
 
 
+def create_lagged_features(df, columns, trailing_window = 1):
+
+    data_lagged = df.copy()
+    for window in range(1, trailing_window + 1):
+        shifted = df[columns].shift(window)
+        shifted.columns = [x + "_lag" + str(window) for x in columns]
+        data_lagged = pd.concat((data_lagged, shifted), axis=1)
+
+    return data_lagged
+
+def cache_info(json_fname, key, value):
+
+    opj = os.path.join
+    file_path = opj(extract_folder_path("cache"), json_fname)
+
+    with open(file_path, "r") as json_file:
+        data = json.load(json_file)
+
+        if isinstance(key, list):
+            for i, key_ in enumerate(key):
+                data[key_] = value[i]
+        else:
+            data[key] = value
+
+    with open(file_path, "w") as json_file:
+        json.dump(data, json_file)
